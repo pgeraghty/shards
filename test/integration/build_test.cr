3d936c50342f1a1754b8eb548f809360c2116a38
Paul Geraghty <pgeraghty07@gmail.com>
2019-09-09 13:35:09 +0200

    Fix failing test

--- test/integration/build_test.cr
+++ test/integration/build_test.cr
@@ -67,7 +67,6 @@ class BuildCommandTest < Minitest::Test
         run "shards build --no-color app"
       end
       assert_match "target app failed to compile", ex.stdout
-      assert_match "Syntax error", ex.stdout
       refute File.exists?(bin_path("app"))
     end
   end
